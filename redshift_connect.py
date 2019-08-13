10409f2279bafd8aa7987fbbee663405c1b32bfd
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2019-08-13 09:47:58 -0400

    Add files via upload

---
+++ redshift_connect.py
@@ -0,0 +1,28 @@
+dbname = 'dev'
+port = '5439'
+user = 'titliaws'
+password = ''
+aws_access_key_id = ''
+aws_secret_access_key = ''
+host = 'redshift-cluster-1.c8f9su7uxkwy.us-west-2.redshift.amazonaws.com'
+host1 = 'aws_iam_role=arn:aws:iam::704795387860:role/RedshiftCopyUnload' #myRedshiftRole
+
+import psycopg2
+#Amazon Redshift connect string
+conn_string = "dbname='dev' port= '5439' user= 'titliaws' password= '' \
+            host= 'training007.cwgovfgtdjch.us-west-2.redshift.amazonaws.com'"
+con = psycopg2.connect(conn_string);
+#to_table = 'users'
+to_table = 'tweets'
+#fn = 's3://awssampledbuswest2/tickit/allusers_pipe.txt'
+fn = 's3://kaggletest/twitter_loads/twitter.csv'
+delim = '|'
+sql="""COPY %s FROM '%s' credentials '%s'
+       delimiter '%s' region 'us-west-2';""" % (to_table, fn, host1, delim)
+cur = con.cursor()
+cur.execute(sql)
+cur.execute("commit;")
+print("Copy executed fine!")
+
+
+
